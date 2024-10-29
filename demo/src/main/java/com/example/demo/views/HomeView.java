c421a74adbadd130e341ce01f4d040b07616cf8b
Tobias Koch <tobias.koch@qbic.uni-tuebingen.de>
2024-10-29 12:05:40 +0100

    example implementation

--- demo/src/main/java/com/example/demo/views/HomeView.java
+++ demo/src/main/java/com/example/demo/views/HomeView.java
@@ -3,6 +3,7 @@ package com.example.demo.views;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.dataview.GridListDataView;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.html.Span;
@@ -11,7 +12,10 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.Route;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Route("")
 public class HomeView extends VerticalLayout {
@@ -33,19 +37,18 @@ public class HomeView extends VerticalLayout {
         contactInfoGrid.addColumn(ContactInfo::name)
             .setHeader("Name");
         contactInfoGrid.addColumn(ContactInfo::email)
-                .setHeader("Email")
-            .setEditorComponent(this::renderContactInfo);
+                .setHeader("Email");
         contactInfoGrid.addComponentColumn(it -> {
             Button button = new Button("Edit");
             button.addClickListener(buttonClickEvent -> contactInfoGrid.getEditor().editItem(it));
             return button;
-        });
-        contactInfoGrid.setItems(List.of(
-            new ContactInfo("John Doe", "john.doe@example.com"),
-            new ContactInfo("John Doe", "john.doe@example.com"),
-            new ContactInfo("John Doe", "john.doe@example.com"),
-            new ContactInfo("John Doe", "john.doe@example.com")
-        ));
+        }).setEditorComponent(this::renderContactInfo);
+        contactInfoGrid.setItems(Stream.of(
+            new ContactInfo("John Doe1", "john.doe@example.com"),
+            new ContactInfo("John Doe2", "john.doe@example.com"),
+            new ContactInfo("John Doe3", "john.doe@example.com"),
+            new ContactInfo("John Doe4", "john.doe@example.com")
+        ).collect(Collectors.toCollection(() -> new ArrayList<>())));
         add(contactInfoGrid);
 
     }
@@ -53,17 +56,19 @@ public class HomeView extends VerticalLayout {
     private Component renderContactInfo(ContactInfo contactInfo) {
         var textField = new TextField();
         textField.setValue(contactInfo.name());
-        textField.addValueChangeListener(
-            contact -> contactInfoGrid.setItems(
-                List.of(new ContactInfo(contact.getValue(), contactInfo.name()))
-                //more complex stuff happening
-            ));
         var textFieldEmail = new TextField();
         textFieldEmail.setValue(contactInfo.email());
-        textFieldEmail.addValueChangeListener(
-            contact -> contactInfoGrid.setItems(
-                List.of(new ContactInfo(contactInfo.email(), contact.getValue()))));
-        return new Span(textField,textFieldEmail);
+        var okButton = new Button("Save");
+        okButton.addClickListener(buttonClickEvent -> {
+            var listDataView = contactInfoGrid.getListDataView();
+            listDataView.addItemBefore(
+                new ContactInfo(textField.getValue(), textFieldEmail.getValue()), contactInfo);
+            listDataView.removeItem(contactInfo);
+            listDataView.refreshAll();
+            contactInfoGrid.getEditor().closeEditor();
+        });
+        return new Span(textField,textFieldEmail,okButton);
+
 
     }
 }
